9e91eb9a20425abd765e4f2b61d90e397904b296
Ryan Harvey <rkharvey@mekapaedia.com>
2025-08-17 18:07:27 +0100

    init

---
+++ hognose.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from lark import Lark
+
+grammar_text = None
+
+with open("grammar.lark") as f:
+    grammar_text = f.read()
+
+parser = Lark(grammar_text)
+
+input_text = "[2: 500_000.2e-10+3i-5.0j+400k, 3[..2]: [2...4;[2..a[1..]]]"
+
+print(parser.parse(input_text).pretty())
